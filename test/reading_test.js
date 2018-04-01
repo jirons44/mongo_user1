d2aabc97a46fb3152e45f2314249dc57ecb8af2e
jirons44 <joeirons@comcast.net>
2018-04-01 08:54:46 -0500

    find users with .sort .skip .limit for paging list

--- test/reading_test.js
+++ test/reading_test.js
@@ -8,8 +8,14 @@ describe('Reading records', () => {
     beforeEach((done) => {
 
         joe = new User({name: 'Joe'});
-        joe.save()
+        user2 = new User({name: 'Mr. User2'});
+        user3 = new User({name: 'Mr. User3'});
+        user4 = new User({name: 'Mr. User4'});
+        user5 = new User({name: 'Mr. User5'});
+
+        Promise.all([joe.save(), user2.save(),user3.save(), user4.save(), user5.save()])
             .then(() =>  done());
+
     });
 
     it('finds all users with name as Joe', (done) => {
@@ -44,6 +50,34 @@ describe('Reading records', () => {
                 done();
 
             });
+    });
+
+    describe('Reading using skip and limit', () => {
+
+        beforeEach(() => {
+
+        });
+
+        it('find users skip first user and only bring back 2 users', (done) => {
+
+            let searchCriteria = {};
+
+            // sort -1 desc, 1: ascending
+            User.find( searchCriteria )
+                .sort({name: 1})
+                .skip(1)
+                .limit(2)
+                .then((users) => {
+
+                    assert(users.length === 2 );
+                    assert(users[0].name === 'Mr. User2' );
+                    assert(users[1].name === 'Mr. User3' );
+
+                    done();
+
+                });
+        });
+
     });
 
 });
